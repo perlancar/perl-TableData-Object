9a823d3f61b969d264e08a605a8c8605f4b26d97
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-08-14 13:52:00 +0700

    Initial commit

---
+++ lib/TableData/Object.pm
@@ -0,0 +1,229 @@
+package TableData::Object;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use experimental 'smartmatch';
+
+use Data::Check::Structure qw(is_aos is_aoaos is_aohos);
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(table);
+
+sub table { __PACKAGE__->new(@_) }
+
+sub new {
+    my ($class, $data) = @_;
+    if (!defined($data)) {
+        die "Please specify table data";
+    } elsif (is_aos($data, {max=>10})) {
+        TableData::Object::aos->new($data);
+    } elsif (is_aoaos($data, {max=>10})) {
+        TableData::Object::aoaos->new($data);
+    }elsif (is_aohos($data, {max=>10})) {
+        TableData::Object::aohos->new($data);
+    } else {
+        die "Unknown table data form, please supply array of scalar, ".
+            "array of array of scalar, or array of hash of scalar";
+    }
+}
+
+sub as_aoaos { shift->rows_as_array(@_) }
+
+sub as_aohos { shift->rows_as_hash(@_) }
+
+sub columns {
+    my ($self, $val) = @_;
+    if ($val) {
+        die "Number of columns must stay the same"
+            unless @$val == @{$self->{columns}};
+        my $oldval = $self->{columns};
+        $self->{columns} = $val;
+        return $oldval;
+    }
+    $self->{columns};
+}
+
+
+package
+    TableData::Object::aos;
+our @ISA = qw(TableData::Object);
+
+sub new {
+    my ($class, $data) = @_;
+    bless {columns=>["data"], data=>$data}, $class;
+}
+
+sub rows_as_array {
+    my $self = shift;
+    [ map {[$_]} @{$self->{data}} ];
+}
+
+sub rows_as_hash {
+    my $self = shift;
+    my $cols = $self->{columns};
+    [ map {{$cols->[0] => $_}} @{$self->{data}} ];
+}
+
+
+package
+    TableData::Object::aoaos;
+our @ISA = qw(TableData::Object);
+
+sub new {
+    my ($class, $data) = @_;
+    bless {columns=>[map {"column$_"} 0..@{$data->[0]}-1], data=>$data}, $class;
+}
+
+sub rows_as_array { shift->{data} }
+
+sub rows_as_hash {
+    my $self = shift;
+    my $cols = $self->{columns};
+    my @res;
+    for my $row (@{ $self->{data} }) {
+        my $hos = { map { $cols->[$_] => $row->[$_] } 0..@$cols-1 };
+        push @res, $hos;
+    }
+    \@res;
+}
+
+
+package
+    TableData::Object::aohos;
+our @ISA = qw(TableData::Object);
+
+sub new {
+    my ($class, $data) = @_;
+    my %cols;
+    for my $row (@$data) {
+        $cols{$_}++ for keys %$row;
+    }
+    bless {columns=>[sort keys %cols], data=>$data}, $class;
+}
+
+sub columns {
+    my ($self, $val) = @_;
+    if ($val) {
+        die "Setting columns for aohos not yet implemented";
+    }
+    $self->{columns};
+}
+
+sub rows_as_array {
+    my $self = shift;
+    my $cols = $self->{columns};
+    my @res;
+    for my $row (@{ $self->{data} }) {
+        my $aos = [ map { $row->{$_} } @$cols ];
+        push @res, $aos;
+    }
+    \@res;
+}
+
+sub rows_as_hash { shift->{data} }
+
+1;
+# ABSTRACT: Manipulate table data
+
+=head1 SYNOPSIS
+
+ use TableData::Object qw(table);
+
+ $td = TableData::Object->new([1, 2, 3, 4]);  # from array of scalars
+ $td = TableData::Object->new([[1,2],[2,3]]); # from array of arrays of scalars
+ $td = TableData::Object->new([{name=>"Andi"}, {name=>"Budi", gender=>"m"}]);
+                                              # from array of hashes of scalars
+ $td = TableData::Object->new(4); # die, can only accept in the above form
+
+ # shortcut to construct object
+ $td = table(...);
+
+ # for the examples below, this object is assumed
+ $td = TableData::Object->new([["Andi",3000], ["Budi",4000], ["Cinta",2500]]);
+
+ # retrieve names of columns
+ $cols = $td->columns; # -> ["column0", "column1"]
+
+ # set names of columns
+ $td->columns(["name", "salary"]);
+
+ # retrieve rows data, each row as arrays
+ $rows = $td->rows_as_array; # -> (["Andi",3000], ["Budi",4000], ["Cinta",2500])
+
+ # retrieve rows data, each row as hash
+ $rows = $td->rows_as_hash; # -> ({name=>"Andi",salary=>3000}, {name=>"Budi",salary=>4000}, {name=>"Cinta",salary=>2500})
+
+ # retrieve a specific row
+ $row = $td->row_as_array(2); # -> ["Cinta",2500]
+ $row = $td->row_as_hash(1); # -> [{name=>"Budi",salary=>4000}]
+
+ # convert to specific forms
+ $data = $td->as_aoaos;
+ $data = $td->as_aohos;
+
+ # XXX add row
+
+ # XXX add column
+
+ # XXX delete row(s)
+
+ # XXX delete column(s)
+
+ # XXX rename column
+
+ # XXX reorder column
+
+
+=head1 DESCRIPTION
+
+This module provides a class to manipulate table data. Table data can be in the
+form of array of scalars (aos), array of arrays of scalars (aoaos), or array of
+hashes of scalars (aohos). There are methods to get/set the columns/rows,
+convert to the other forms, etc.
+
+Aos data is assumed to be a single-column table with column named C<data> (but
+this can be renamed). Aoaos data is assumed to have columns named C<column0>,
+C<column1>, and so on (but this can be changed). Aohos data is assumed to have
+columns according to the hash keys (sorted alphabetically) and column names
+cannot be changed.
+
+
+=head1 FUNCTIONS
+
+=head2 table($data) => obj
+
+Exportable. Shortcut for constructor.
+
+
+=head1 METHODS
+
+=head2 columns([ $cols ]) => array
+
+Get or set columns.
+
+=head2 rows_as_array => array of array of scalar
+
+=head2 rows_as_hash => array of hash of scalar
+
+=head2 row_as_array($index) => array of scalar
+
+=head2 row_as_hash($index) => hash of scalar
+
+=head2 as_aoaos() => array of array of scalar
+
+=head2 as_aohos() => array of hash of scalar
+
+
+=head1 TODO
+
+Specify table spec (L<TableDef>) to preset column names and orders.
+
+
+=head1 SEE ALSO
+
+L<TableDef>
---
+++ t/01-basics.t
@@ -0,0 +1,66 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use TableData::Object qw(table);
+use Test::Exception;
+use Test::More 0.98;
+
+dies_ok { table() } "unknown form 1";
+dies_ok { table(1) } "unknown form 2";
+
+subtest aos => sub {
+    my $td = table([10, 11, 12]);
+
+    ok($td->isa("TableData::Object::aos"), "class");
+    is_deeply($td->columns, ["data"], "default columns");
+
+    dies_ok { $td->columns(["num","num2"]) }
+        "new columns must have same number of columns";
+    $td->columns(["num"]);
+    is_deeply($td->columns, ["num"], "set columns");
+
+    is_deeply($td->rows_as_array, [[10],[11],[12]], "rows_as_array");
+    is_deeply($td->as_aoaos     , [[10],[11],[12]], "as_aoaos");
+    is_deeply($td->rows_as_hash , [{num=>10},{num=>11},{num=>12}], "rows_as_hash");
+    is_deeply($td->as_aohos     , [{num=>10},{num=>11},{num=>12}], "as_aohos");
+};
+
+subtest aoaos => sub {
+    my $td = table([["andi",3000],["budi",4000],["citra",2500]]);
+
+    ok($td->isa("TableData::Object::aoaos"), "class");
+    is_deeply($td->columns, ["column0","column1"], "default columns");
+
+    dies_ok { $td->columns(["nom"]) }
+        "new columns must have same number of columns";
+    $td->columns(["name","salary"]);
+    is_deeply($td->columns, ["name","salary"], "set columns");
+
+    is_deeply($td->rows_as_array, [["andi",3000],["budi",4000],["citra",2500]], "rows_as_array");
+    is_deeply($td->as_aoaos     , [["andi",3000],["budi",4000],["citra",2500]], "as_aoaos");
+    is_deeply($td->rows_as_hash , [{name=>"andi",salary=>3000},{name=>"budi",salary=>4000},{name=>"citra",salary=>2500}], "rows_as_hash");
+    is_deeply($td->as_aohos     , [{name=>"andi",salary=>3000},{name=>"budi",salary=>4000},{name=>"citra",salary=>2500}], "as_aohos");
+};
+
+subtest aohos => sub {
+    my $td = table([{name=>"andi",salary=>3000},
+                    {name=>"budi",salary=>4000,note=>"test"},
+                    {name=>"citra",salary=>2500}]);
+
+    ok($td->isa("TableData::Object::aohos"), "class");
+    is_deeply($td->columns, ["name","note","salary"], "default columns");
+
+    # TODO: set columns not yet implemented
+
+    is_deeply($td->rows_as_array, [["andi",undef,3000],["budi","test",4000],["citra",undef,2500]], "rows_as_array");
+    is_deeply($td->as_aoaos     , [["andi",undef,3000],["budi","test",4000],["citra",undef,2500]], "rows_as_array");
+    is_deeply($td->rows_as_hash , [{name=>"andi",salary=>3000},{name=>"budi",salary=>4000,note=>"test"},{name=>"citra",salary=>2500}], "rows_as_hash");
+    is_deeply($td->as_aohos     , [{name=>"andi",salary=>3000},{name=>"budi",salary=>4000,note=>"test"},{name=>"citra",salary=>2500}], "as_aohos");
+};
+
+DONE_TESTING:
+done_testing;
+
