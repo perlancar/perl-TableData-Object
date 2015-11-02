8fab57aec1abc46e2e4ff811721f14caba2af39d
perlancar (@pc-home) <perlancar@gmail.com>
2015-11-02 07:52:43 +0700

    Rework documentation

--- lib/TableData/Object/Base.pm
+++ lib/TableData/Object/Base.pm
@@ -60,19 +60,35 @@ sub col_count {
 }
 
 sub _select {
-    my ($self, $cols, $func_filter_row) = @_;
+    my ($self, $_as, $cols, $func_filter_row) = @_;
 
     my $spec = {fields=>{}};
     my $i = 0;
-    for (@$cols) {
-        $spec->{fields}{$_} = {pos=>$i};
+    for my $col (@$cols) {
+        die "Column '$col' does not exist" unless $self->col_exists($col);
+        $spec->{fields}{$col} = {%{$self->{spec}{fields}{$col} // {}}, pos=>$i};
         $i++;
     }
     my $data = [];
 
-    for my $row0 (@{ $self->rows }) {
-        #next unless $func_filter_row->($row);
+    for my $row (@{ $self->rows_as_aohos }) {
+        next unless !$func_filter_row || $func_filter_row->($self, $row);
+        # select columns
+        for (keys %$row) {
+
+        }
+        push @$data, $row;
     }
+}
+
+sub select_as_aoaos {
+    my ($self, $cols, $func_filter_row) = @_;
+    $self->_select('aoaos', $cols, $func_filter_row);
+}
+
+sub select_as_aohos {
+    my ($self, $cols, $func_filter_row) = @_;
+    $self->_select('aohos', $cols, $func_filter_row);
 }
 
 1;
@@ -87,15 +103,51 @@ L<TableDef>.
 
 =head2 $td->cols_by_name => hash
 
+Return the columns as a hash with name as keys and index as values.
+
+Example:
+
+ {name=>0, gender=>1, age=>2}
+
 =head2 $td->cols_by_idx => array
+
+Return the columns as an array where the element will correspond to the column's
+position.
+
+Example:
+
+ ["name", "gender", "age"]
 
 =head2 $td->row_count() => int
 
 Return the number of rows.
 
+See also: C<col_count()>.
+
 =head2 $td->col_count() => int
 
 Return the number of columns.
+
+See also: C<row_count()>.
+
+=head2 $td->col_exists($name_or_idx) => bool
+
+Check whether a column exists. Column can be referred to using its name or
+index/position (0, 1, ...).
+
+=head2 $td->col_name($idx) => str
+
+Return the name of column referred to by its index/position. Undef if column is
+unknown.
+
+See also: C<col_idx()>.
+
+=head2 $td->col_idx($name) => int
+
+Return the index/position of column referred to by its name. Undef if column is
+unknown.
+
+See also: C<col_name()>.
 
 =head2 $td->sort_rows(@sortcols) => obj
 
@@ -108,22 +160,35 @@ Example:
  # sort employees from oldest to youngest, then by name
  $td->sort_rows("-age", "name")
 
-=head2 $td->rows() => array
+If you have a C<::hash> or C<::aos> object, an C<::aoaos> object will be
+returned instead. Otherwise, object of the same type will be returned.
 
-Return an array(ref) of rows. In C<::hash>, will return C<< [ [key,val], ... ]
->>, and in other forms will return the raw C<data>.
+=head2 $td->rows_as_aoaos() => aoaos
 
-=head2 $td->col_exists($name_or_idx) => bool
+Return rows as array of array-of-scalars.
 
-Check whether a column exists. Column can be referred to using its name or
-index/position (0, 1, ...).
+See also: C<rows_as_aohos()>.
 
-=head2 $td->col_name($idx) => str
+=head2 $td->rows_as_aohos() => aohos
 
-Return the name of column referred to by its index/position. Undef if column is
-unknown.
+Return rows as array of hash-of-scalars.
 
-=head2 $td->col_idx($name) => int
+See also: C<rows_as_aoaos()>.
 
-Return the index/position of column referred to by its name. Undef if column is
-unknown.
+=head2 $td->select_as_aoaos($cols[ , $func_filter_row ]) => aoaos
+
+Like C<rows_as_aoaos()>, but allow selecting columns and filtering rows.
+C<$func_filter_row> is a coderef that will be passed C<< ($td, $row_as_hos) >>
+and should return true/false depending on whether the row should be included in
+the resultset.
+
+See also: C<select_as_aohos()>.
+
+=head2 $td->rows_as_aohos($cols[ , $func_filter_row ]) => aohos
+
+Like C<rows_as_aohos()>, but allow selecting columns and filtering rows.
+C<$func_filter_row> is a coderef that will be passed C<< ($td, $row_as_hos) >>
+and should return true/false depending on whether the row should be included in
+the resultset.
+
+See also: C<select_as_aoaos()>.
